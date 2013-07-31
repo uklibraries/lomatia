d8907e006eaaedc34e220c94427c90d43f764eae
Michael Slone <m.slone@gmail.com>
2013-07-31 15:33:07 -0400

    allow validation errors for failing bags to be recorded

--- lib/lomatia/worker/check_fixity/bag.rb
+++ lib/lomatia/worker/check_fixity/bag.rb
@@ -13,10 +13,21 @@ module Lomatia
           bag = BagIt::Bag.new node
           log = options['log']
 
+          validity = bag.valid? ? "valid" : "invalid"
           File.open(log, 'a') do |f|
-            validity = bag.valid? ? "valid" : "invalid"
             puts "#{identifier}: #{validity}"
             f.puts "#{identifier}: #{validity}"
+          end
+
+          if options['error'] and validity == "invalid"
+            File.open(errors, 'a') do |f|
+              f.puts '=' * 70
+              f.puts identifier
+              f.puts node
+              f.puts 
+              f.puts bag.errors.full_messages.join("\n")
+              f.puts
+            end
           end
         end
       end
