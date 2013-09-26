53ec7e634e920e014c096c89dc585b5de057811d
Michael Slone <m.slone@gmail.com>
2013-09-26 16:31:51 -0400

    fill out launch remote job worker

--- lib/launch_remote_job.rb
+++ lib/launch_remote_job.rb
@@ -1,8 +1,26 @@
+require 'base64'
+require 'net/ssh'
+require 'yaml'
+
 class LaunchRemoteJob
   @queue = :ingest
 
   def self.perform payload
-    # do nothing
-    puts payload
+    file = File.join File.dirname(File.dirname(__FILE__)),
+                     'config',
+                     'remote-hosts.yml'
+    config = YAML.load_file(file)
+
+    server = payload['server']['name']
+
+    if config.has_key? server
+      Net::SSH.start(server, config[server]['username']) do |ssh|
+        serialized = Base64.strict_encode64(payload.to_json)
+        output = ssh.exec!("#{config[server]['create_job']} #{serialized}")
+        puts output
+      end
+    else
+      # TODO: fail task
+    end
   end
 end
