769935a851128952d2933dcf38369339a7f5a0dd
Michael Slone <m.slone@gmail.com>
2013-11-14 08:19:07 -0500

    gather more statistics

--- lib/lomatia/worker/gather_title_statistics/bag.rb
+++ lib/lomatia/worker/gather_title_statistics/bag.rb
@@ -1,7 +1,9 @@
 require 'bundler/setup'
 require 'bagit'
 require 'csv'
+require 'find'
 require 'nokogiri'
+require 'time'
 
 class BagAnalyzer
   def initialize options
@@ -29,6 +31,43 @@ class BagAnalyzer
       File.size(File.join(@node, 'data', f['xlink:href']))
     }.inject(&:+)
   end
+
+  def total_size
+    size = 0
+    Find.find(@node) do |path|
+      if File.file? path
+        size += File.size(path)
+      end
+    end
+    size
+  end
+
+  def timestamp
+    Time.parse(
+      @xml.xpath('//metsHdr').first['LASTMODDATE']
+    ).utc.iso8601
+  end
+
+  # This method of distinguishing AIPs from DIPs does NOT
+  # generalize to arbitrary objects in the KUDL repository,
+  # but it works for the born-PDF newspaper objects for which
+  # it was intended.  At the time this comment was written,
+  # the METS profile for KUDL objects did not require that
+  # AIP/DIP type be declared within the METS file.
+  #
+  # --mps 2013-11-14
+  def type
+    result = 'aip'
+    Find.find(File.join(@node, 'data')) do |path|
+      if File.file? path
+        if '.jpg' == File.extname(path)
+          result = 'dip'
+          break
+        end
+      end
+    end
+    result
+  end
 end
 
 module Lomatia
@@ -53,10 +92,13 @@ module Lomatia
               csv << [
                 identifier,
                 node,
+                analyzer.type,
+                analyzer.timestamp,
                 analyzer.title,
                 analyzer.page_count,
                 analyzer.total_pdf_size,
                 analyzer.total_tiff_size,
+                analyzer.total_size,
               ]
             end
           end
