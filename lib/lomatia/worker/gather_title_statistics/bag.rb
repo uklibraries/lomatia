48ac5bf42e40e594a6675527341ae4860c908f7d
Michael Slone <m.slone@gmail.com>
2014-01-14 16:00:37 -0500

    allow broad AIP/DIP store queries instead of only title-focused queries

--- lib/lomatia/worker/gather_title_statistics/bag.rb
+++ lib/lomatia/worker/gather_title_statistics/bag.rb
@@ -49,24 +49,16 @@ class BagAnalyzer
   end
 
   # This method of distinguishing AIPs from DIPs does NOT
-  # generalize to arbitrary objects in the KUDL repository,
-  # but it works for the born-PDF newspaper objects for which
-  # it was intended.  At the time this comment was written,
-  # the METS profile for KUDL objects did not require that
-  # AIP/DIP type be declared within the METS file.
+  # generalize to arbitrarily-named nodes, but it works 
+  # for the naming scheme used for nodes in the KUDL system.
   #
-  # --mps 2013-11-14
+  # --mps 2014-01-14
   def type
-    result = 'aip'
-    Find.find(File.join(@node, 'data')) do |path|
-      if File.file? path
-        if '.jpg' == File.extname(path)
-          result = 'dip'
-          break
-        end
-      end
+    if @node.include? 'aips'
+      'aip'
+    else
+      'dip'
     end
-    result
   end
 end
 
@@ -81,26 +73,30 @@ module Lomatia
           identifier = File.basename node
           bag = BagIt::Bag.new node
           log = options['log']
-          titles_file = options['titles']
 
-          title_count = `grep -c -f #{titles_file} #{File.join(node, 'data', 'mets.xml')}`.chomp.to_i
+          if options['titles']
+            titles_file = options['titles']
+            title_count = `grep -c -f #{titles_file} #{File.join(node, 'data', 'mets.xml')}`.chomp.to_i
 
-          if title_count > 0
-            analyzer = BagAnalyzer.new :node => node
-            CSV.open(log, 'a') do |csv|
-              puts "#{identifier}"
-              csv << [
-                identifier,
-                node,
-                analyzer.type,
-                analyzer.timestamp,
-                analyzer.title,
-                analyzer.page_count,
-                analyzer.total_pdf_size,
-                analyzer.total_tiff_size,
-                analyzer.total_size,
-              ]
+            if title_count == 0
+              return
             end
+          end
+
+          analyzer = BagAnalyzer.new :node => node
+          CSV.open(log, 'a') do |csv|
+            puts "#{identifier}"
+            csv << [
+              identifier,
+              node,
+              analyzer.type,
+              analyzer.timestamp,
+              analyzer.title,
+              analyzer.page_count,
+              analyzer.total_pdf_size,
+              analyzer.total_tiff_size,
+              analyzer.total_size,
+            ]
           end
         end
       end
